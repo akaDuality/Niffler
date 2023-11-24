ca127df5860fbe89a6177094f180ee53ba7bbd44
Mikhail Rubanov <akaDuality@yandex.ru>
2023-11-24 09:49:13 -0300

    Simlify model for spends

--- Niffler/Niffler/VIew/SpendsView.swift
+++ Niffler/Niffler/VIew/SpendsView.swift
@@ -2,9 +2,24 @@ import Api
 import SwiftUI
 
 struct SpendsView: View {
-    @ObservedObject var spendsModel = SpendsViewModel()
     @State var spends: [Spends] = []
 
+    let network = Api()
+    
+    func fetchData() {
+        Task {
+            try await network.auth.authorize(user: "stage", password: "12345")
+            
+            let (spends, response) = try await network.getSpends()
+            
+            await MainActor.run {
+                self.spends = spends
+            }
+        }
+    }
+}
+
+extension SpendsView {
     var body: some View {
         VStack {
             List(spends) { spend in
@@ -15,27 +30,11 @@ struct SpendsView: View {
             Text("Spends View?")
         }
         .onAppear {
-            Task {
-                let spends = try await spendsModel.fetchData()
-                
-                await MainActor.run {
-                    self.spends = spends
-                }
-            }
+            fetchData()
         }
     }
 }
 
-class SpendsViewModel: ObservableObject {
-    let network = Api()
-
-    func fetchData() async throws -> [Spends] {
-        try await network.auth.authorize(user: "stage", password: "12345")
-
-        let (spends, response) = try await network.getSpends()
-        return spends
-    }
-}
 
 #Preview {
     SpendsView()
