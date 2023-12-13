c24ffa4daabc4d2c526b7f9aab413c3cf7b2c05f
s.karpenko <s.karpenko@dodopizza.com>
2023-12-13 13:53:38 +0700

    move DateFormatterHelper to file

--- Niffler/Api/Sources/Api/Spends+SpendsDTO.swift
+++ Niffler/Api/Sources/Api/Spends+SpendsDTO.swift
@@ -18,34 +18,3 @@ extension Spends {
         self.username = dto.username
     }
 }
-
-public class DateFormatterHelper {
-    private let dateFormatterInput: DateFormatter
-    private let dateFormatterOutput: DateFormatter
-    
-    public init() {
-        dateFormatterInput = DateFormatter()
-        dateFormatterInput.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-
-        dateFormatterOutput = DateFormatter()
-        dateFormatterOutput.dateFormat = "dd MMM yy"
-    }
-    
-    func formatDateString(_ dateString: String) -> String {
-        if let date = dateFormatterInput.date(from: dateString) {
-            return dateFormatterOutput.string(from: date)
-        } else {
-            return "NoData"
-        }
-    }
-    
-    public func formatToApi(_ date: Date) -> String {
-        dateFormatterInput.string(from: date)
-    }
-    
-    public func formatForApiAddSpend(_ dateForm: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
-        return formatter.string(from: dateForm)
-    }
-}
