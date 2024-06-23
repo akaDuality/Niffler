e3be44b47028e005e1ea9cf9803aca9fb65b34de
s.karpenko <s.karpenko@dodobrands.io>
2024-06-23 15:26:55 +0500

    using some color in Chart and Legend

--- Niffler/Niffler/VIew/StatisticView.swift
+++ Niffler/Niffler/VIew/StatisticView.swift
@@ -4,6 +4,13 @@ import SwiftUI
 
 struct StatisticView: View {
     @Binding var spends: [Spends]
+    @State private var groupedSpendsWithColor: GroupedSpendsWithColor
+
+    init(spends: Binding<[Spends]>) {
+        _spends = spends
+        _groupedSpendsWithColor = State(initialValue: GroupedSpendsWithColor(spends: spends.wrappedValue))
+    }
+
     var totalAmount: Double { spends.reduce(0.0) { result, spend in
         result + spend.amount
     }}
@@ -18,36 +25,41 @@ struct StatisticView: View {
             }
 
             HStack {
-                Chart(spends) { spend in
-                    SectorMark(
-                        angle: .value("Amount", spend.amount),
-                        innerRadius: .ratio(0.8),
-                        outerRadius: .inset(10)
-                    )
-                    .foregroundStyle(Color.random)
-                }
-                .overlay(
-                    Text("\(totalAmount, specifier: "%.0f") ₽")
-                        .bold()
-                        .foregroundColor(.black)
-                )
-
+                CustomChart(spends: spends)
                 Legend(spends: spends)
             }
         }
     }
 
     @ViewBuilder
+    func CustomChart(spends: [Spends]) -> some View {
+        Chart(groupedSpendsWithColor.array) { spend in
+            SectorMark(
+                angle: .value("Amount", spend.amount),
+                innerRadius: .ratio(0.8),
+                outerRadius: .inset(10)
+            )
+            .foregroundStyle(spend.color)
+        }.overlay(
+            Text("\(totalAmount, specifier: "%.0f") ₽")
+                .bold()
+                .foregroundColor(.black)
+        )
+    }
+
+    @ViewBuilder
     func Legend(spends: [Spends]) -> some View {
         VStack {
-            ForEach(sortedCategory(spends: spends), id: \.0) { key, value in
-                CategoryLabel(key, value)
+            ForEach(groupedSpendsWithColor.array) { spend in
+                CategoryLabel(spend.category,
+                              spend.amount,
+                              spend.color)
             }
         }
     }
 
     @ViewBuilder
-    func CategoryLabel(_ category: String, _ amount: Double) -> some View {
+    func CategoryLabel(_ category: String, _ amount: Double, _ color: Color) -> some View {
         HStack {
             Text("\(category) \(amount, specifier: "%.0f") ₽")
                 .foregroundColor(.white)
@@ -55,7 +67,7 @@ struct StatisticView: View {
         }
         .padding(.horizontal)
         .padding(.vertical, 8)
-        .background(Color.random)
+        .background(color)
         .cornerRadius(25)
     }
 }
@@ -70,18 +82,40 @@ extension Color {
     }
 }
 
-extension StatisticView {
-    func sortedCategory(spends: [Spends]) -> [(String, Double)] {
-        var categoryTotals: [String: Double] = [:]
+struct GroupedSpendsWithColor {
+    var array: [GroupedSpends]
+    struct GroupedSpends: Identifiable {
+        var id: UUID
 
-        for spend in spends {
-            categoryTotals[spend.category, default: 0.0] += spend.amount
-        }
-
-        let sortedCategoryTotals = categoryTotals.sorted(by: { $0.key < $1.key })
-
-        return sortedCategoryTotals
+        var category: String
+        var amount: Double
+        var color: Color
     }
+
+    init(spends: [Spends]) {
+        let staticSpends = sortedCategory(spends: spends)
+
+        array = staticSpends.map { category, amount, color in
+            GroupedSpends(id: UUID(), category: category, amount: amount, color: color)
+        }
+    }
+}
+
+private func sortedCategory(spends: [Spends]) -> [(String, Double, Color)] {
+    var categoryTotals: [String: Double] = [:]
+
+    for spend in spends {
+        categoryTotals[spend.category, default: 0.0] += spend.amount
+    }
+
+    let sortedCategoryTotals = categoryTotals.sorted(by: { $0.key < $1.key })
+    var categorizedData: [(String, Double, Color)] = []
+    for (category, total) in sortedCategoryTotals {
+        let randomColor = Color.random
+        categorizedData.append((category, total, randomColor))
+    }
+
+    return categorizedData
 }
 
 #Preview {
